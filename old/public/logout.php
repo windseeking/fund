b1400ab875d8a781c69740bdcae7429462c27459
Инна Клементьева <windseeking2@gmail.com>
2021-08-11 00:45:02 +0300

    Deleted some trash

--- old/public/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['user']);
-$_SESSION['logout'] = /** @lang text */
-    'You have successfully logged out.<br> See you soon!';
-header("Location: login");
--- old/public/templates/email.php
+++
@@ -1,6 +0,0 @@
-<h1>С Вами связались через форму на innovationfund.in</h1>
-<p><b>Имя:</b> <?= $contact_name ;?></p>
-<p><b>Email:</b> <?= $contact_email ;?></p>
-<p><?= $contact_message; ?></p>
-<hr>
-<small><a href="http://innovationfund.in" target="_blank">Fund of Innovation Support</a></small>
